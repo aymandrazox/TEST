1ac462e56913afb6156c16a5782cf51f1429c7af
aymandrazox <53756343+aymandrazox@users.noreply.github.com>
2019-08-17 10:29:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("609391541869608960")
+let channel =     client.channels.get("")
 setInterval(function() {
 channel.send(`عطيينيي كرييدتس هاهاهاها بزز منبسرررررررررررعة `);
 }, 30)
